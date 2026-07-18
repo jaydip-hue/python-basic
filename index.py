451f114249fe8c04dfaf3060586eea70081bd042
jaydip-hue <deyjaydip.03@gmail.com>
2026-07-18 11:41:17 +0530

    Add files via upload

--- index.py
+++ index.py
@@ -1,31 +1,98 @@
-print('''Hello World''')
+#Operators
+#addition
+a = 10
+b = 15
+res = a + b
+print (res)
 
-X = 10
-print (X)
 
-b = 5
-print (type(b))
-z = 10.5
-print (type(z))
-c = 2+3j
-print (type(c))
-str1 ="Hello , new String@1234"
-print (str1)
-print (type(str1))
-list1 = ["Delhi", "Noida", "GK"]
-list1.append("Dwarka")
-print (list1)
+#Modulas using % sign, it will give reminder
+a = 20
+b = 20
+res = a%b
+print(res)
 
-tup = ("1","2","3","2")
-print (tup)
+#Power using ** sign
+a = 5
+b = 2
+res = a**b
+print(res)
 
-set1 = {4,4,4,25,37,2,2}
-print (set1)
+#Floor Division using // sign
+a = 5
+b = 2
+res = a//b
+print(res)
 
-dic1 = { 
-    "location": "Lodhi Road",
-    "temperature": 40,
-    "pin": "11003"
-}
+#Comparison Operator using == sign output will come in true or false (data types should be same, if different results will be false)
+a = 5
+b = 2
+res = a==b
+print(res)
 
-print (dic1)
+a = 5
+b = 2
+res = a!=b #! not equals 
+print(res)
+
+a = 5
+b = 2
+res = a>b 
+print(res)
+
+a = 5
+b = 2
+res = a<=b 
+print(res)
+
+#Logical Operators and or not
+num1 = 24
+num2 = 45
+
+print(num1<num2 and num1>=num2) 
+
+print(num1<num2 or num1>=num2)
+
+#Practice
+x = 50
+y = 20
+
+print (x<y and y>x)
+print (x<y or y<x)
+
+print (not True)
+
+#Assignment operator
+a = 4
+a += 5
+print (a)
+
+a = 4
+a = + 5
+print (a)
+
+a = 4
+a = - 5
+print (a)
+
+a = 4
+a *= 5
+print (a)
+
+a /= 5
+print (a)
+
+#Identity Operator (is or is not)
+x = 5
+y = 7
+print (x is y)
+
+x = 5
+y = 7
+print (x is not y)
+
+#Membership Operators
+a=[10,20,35,110,510]
+print (20 not in a)
+print (500 in a)
+print (500 not in a)
