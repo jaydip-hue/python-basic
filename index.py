d56b2bf620cd00858ca224c1b39ccaaf8cdb10b2
jaydip-hue <deyjaydip.03@gmail.com>
2026-07-18 11:21:06 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,31 @@
+print('''Hello World''')
+
+X = 10
+print (X)
+
+b = 5
+print (type(b))
+z = 10.5
+print (type(z))
+c = 2+3j
+print (type(c))
+str1 ="Hello , new String@1234"
+print (str1)
+print (type(str1))
+list1 = ["Delhi", "Noida", "GK"]
+list1.append("Dwarka")
+print (list1)
+
+tup = ("1","2","3","2")
+print (tup)
+
+set1 = {4,4,4,25,37,2,2}
+print (set1)
+
+dic1 = { 
+    "location": "Lodhi Road",
+    "temperature": 40,
+    "pin": "11003"
+}
+
+print (dic1)
